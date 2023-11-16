ab90505ca1debba1885e94696c59e061b9ab1973
Kirk Davis <kirkdavi@amazon.com>
2023-11-16 10:34:56 -0800

    minor edits (i forget what) to the techempower user data scripts

--- UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-TechEmpower-Benchmarks/dotnet8-RC2-ARM64-TechEmpower-aspnet-only.sh
+++ UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-TechEmpower-Benchmarks/dotnet8-RC2-ARM64-TechEmpower-aspnet-only.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 # Install .NET SDK 8.0.100-rc.2 for x64 then run TechEmpower ASP.NET Core benchmarks and PostgreSQL DB
 sudo apt-get update -y
-curl -o dotnet-sdk-8-x64.tar.gz https://download.visualstudio.microsoft.com/download/pr/9144f37e-b370-41ee-a86f-2d2a69251652/bc1d544112ec134184a5aec7f7a1eaf9/dotnet-sdk-8.0.100-rc.2.23502.2-linux-x64.tar.gz
+curl -o dotnet-sdk-8-arm64.tar.gz https://download.visualstudio.microsoft.com/download/pr/0247681a-1a4a-4a32-a1a6-4149d56af27e/5bcbf1d8189c2649b16d27f5199e04a4/dotnet-sdk-8.0.100-rc.2.23502.2-linux-arm64.tar.gz
 mkdir /usr/bin/dotnet
-sudo tar -xzvf dotnet-sdk-8-x64.tar.gz -C /usr/bin/dotnet
+sudo tar -xzvf dotnet-sdk-8-arm64.tar.gz -C /usr/bin/dotnet
 sudo sh -c 'echo "alias dotnet=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export DOTNET_ROOT=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export PATH=$PATH:$DOTNET_ROOT" >> /etc/environment'
 set -a; source /etc/environment; set +a;
 
-sudo /usr/bin/dotnet/dotnet tool install —global dotnet-ef —version 7.0.12
-
 export PATH="$PATH:$HOME/.dotnet/tools/"
+cat << \EOF >> ~/.bash_profile
+# Add .NET Core SDK tools
+export PATH="$PATH:/root/.dotnet/tools"
+EOF
+
+export PATH="$PATH:/home/ubuntu/.dotnet/tools"
+cat << \EOF >> ~/.bash_profile
+# Add .NET Core SDK tools
+export PATH="$PATH:/home/ubuntu/.dotnet/tools"
+EOF
+
+# sudo /usr/bin/dotnet/dotnet tool install -g dotnet-ef --version 7.0.12
+sudo /usr/bin/dotnet/dotnet tool install -g dotnet-ef --version 7.0.12
+
+
+sudo /usr/bin/dotnet/dotnet workload update
 
 # Install postrgreSQL
-apt install postgresql
+sudo apt install postgresql
 sudo -u postgres psql template1
 
 # Configure password
