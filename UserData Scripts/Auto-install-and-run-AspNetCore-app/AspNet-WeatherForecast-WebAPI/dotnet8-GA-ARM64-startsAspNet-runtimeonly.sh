8e6d4881adcef19cd07506f4728d97e6cb10a6b9
Kirk Davis <kirkdavi@amazon.com>
2023-12-06 08:50:47 -0800

    removed obsolete user data scripts

--- UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-WeatherForecast-WebAPI/dotnet8-GA-ARM64-startsAspNet-runtimeonly.sh
+++ UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-WeatherForecast-WebAPI/dotnet8-GA-ARM64-startsAspNet-runtimeonly.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Install ASP.NET Core Runtime 8.0 Release Candidate 1 for x64 then run sample asp.net core app - tested on Ubuntu 22.04
+# Install ASP.NET Core Runtime v8.0 (GA) for ARM64 then run sample asp.net core app - tested on Ubuntu 22.04
 sudo apt-get update -y
-curl -o dotnet-aspnet-8-x64.tar.gz https://download.visualstudio.microsoft.com/download/pr/02f4c6c2-c137-448d-a189-9ee3a3f6f99f/192f2c750089fa0194f04f5a805bd21e/aspnetcore-runtime-8.0.0-rc.1.23421.29-linux-x64.tar.gz
+curl -o dotnet-aspnet-8-arm64.tar.gz https://download.visualstudio.microsoft.com/download/pr/91223e4e-2300-4e8e-9364-09ea1c317294/47fb26a2df5eeee08f77a4d1b720a34a/aspnetcore-runtime-8.0.0-linux-arm64.tar.gz
 mkdir /usr/bin/dotnet
-sudo tar -xzvf dotnet-aspnet-8-x64.tar.gz -C /usr/bin/dotnet
+sudo tar -xzvf dotnet-aspnet-8-arm64.tar.gz -C /usr/bin/dotnet
 sudo sh -c 'echo "alias dotnet=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export DOTNET_ROOT=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export PATH=$PATH:$DOTNET_ROOT" >> /etc/environment'
@@ -12,4 +12,5 @@ curl -o performancetestaspnet8.zip kirk-docs-share.s3-us-west-2.amazonaws.com/pe
 sudo apt install unzip -y
 sudo unzip performancetestaspnet8.zip -d aspnetmvc
 sudo chmod +rwx aspnetmvc
+{ crontab -l; echo "@reboot /usr/bin/dotnet/dotnet /aspnetmvc/EC2BenchmarkingAspNet8.dll --urls=http://0.0.0.0:5000"; } | crontab -
 sudo /usr/bin/dotnet/dotnet aspnetmvc/EC2BenchmarkingAspNet8.dll --urls=http://0.0.0.0:5000
--- UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-WeatherForecast-WebAPI/dotnet8-GA-x86-64-startsAspNet-runtimeonly.sh
+++ UserData Scripts/Auto-install-and-run-AspNetCore-app/AspNet-WeatherForecast-WebAPI/dotnet8-GA-x86-64-startsAspNet-runtimeonly.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Install ASP.NET Core Runtime 8.0 preview 7 for ARM64 then run sample asp.net core app - tested on Ubuntu 22.04
+# Install ASP.NET Core Runtime v8.0 (GA) for x64 then run sample asp.net core app - tested on Ubuntu 22.04
 sudo apt-get update -y
-curl -o dotnet-aspnet-8-arm64.tar.gz https://download.visualstudio.microsoft.com/download/pr/7fedb243-5d2c-4718-b08b-da0dc9d32973/f02a41417d762839b4d1559610485727/aspnetcore-runtime-8.0.0-preview.7.23375.9-linux-arm64.tar.gz
+curl -o dotnet-aspnet-8-x64.tar.gz https://download.visualstudio.microsoft.com/download/pr/257bdcc7-cbfd-4680-964a-cbe8e9160bca/ac0cbf19d897ba51ae004b4146940a0a/aspnetcore-runtime-8.0.0-linux-x64.tar.gz
 mkdir /usr/bin/dotnet
-sudo tar -xzvf dotnet-aspnet-8-arm64.tar.gz -C /usr/bin/dotnet
+sudo tar -xzvf dotnet-aspnet-8-x64.tar.gz -C /usr/bin/dotnet
 sudo sh -c 'echo "alias dotnet=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export DOTNET_ROOT=/usr/bin/dotnet" >> /etc/environment'
 sudo sh -c 'echo "export PATH=$PATH:$DOTNET_ROOT" >> /etc/environment'
@@ -12,4 +12,5 @@ curl -o performancetestaspnet8.zip kirk-docs-share.s3-us-west-2.amazonaws.com/pe
 sudo apt install unzip -y
 sudo unzip performancetestaspnet8.zip -d aspnetmvc
 sudo chmod +rwx aspnetmvc
+{ crontab -l; echo "@reboot /usr/bin/dotnet/dotnet /aspnetmvc/EC2BenchmarkingAspNet8.dll --urls=http://0.0.0.0:5000"; } | crontab -
 sudo /usr/bin/dotnet/dotnet aspnetmvc/EC2BenchmarkingAspNet8.dll --urls=http://0.0.0.0:5000
